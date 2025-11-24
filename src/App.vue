f23cf813d44fd758a47ab5c9c3e8197a9775d3b8
edouard herrengt <herrengt.edouard@gmail.com>
2025-11-24 14:56:50 +0100

    Add DisplayButton component and update App.vue; refactor styles in style.css

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
-<script setup></script>
+<script setup>
+import DisplayButton from './components/DisplayButton.vue'
+</script>
 
-<template></template>
+<template>
+  <DisplayButton />
+</template>
---
+++ src/components/DisplayButton.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <button
+    class="relative bg-white text-blue-900 tp-4 uppercase rounded-full cursor-pointer aspect-square w-[272px] max-[800px]:w-[144px] 
+    after:content-[''] after:absolute after:left-1/2 after:top-1/2 after:-translate-x-1/2 
+    after:-translate-y-1/2 after:w-[410px] after:aspect-square after:rounded-full after:bg-white/15 after:opacity-0 
+    after:scale-0 after:transition-all after:duration-300 after:ease-out hover:after:opacity-100 
+    hover:after:scale-100 max-[800px]:after:w-[210px]"
+  >
+    explore
+  </button>
+</template>
